d35837def777bfd2805e8b1284769ecb9fc4eeae
Babar <babarsmoke24@gmail.com>
2025-07-19 21:40:59 +0530

    feat : Initial Commit

---
+++ backend/src/index.ts
@@ -0,0 +1,6 @@
+//TODO : User can track theri finances and can have todo pending purchases with nice charts and expected time
+//TODO : Might migrate to SQL
+import express from "express";
+const PORT = 9000;
+const app = express();
+app.listen(PORT, () => console.log(`Server started Successfully at ${PORT}`));
