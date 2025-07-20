575a1aa8255b35d4126e58c0ef18c392d844be39
Babar <babarsmoke24@gmail.com>
2025-07-20 18:57:57 +0530

    Refactor : Refactored the Routes function to allow prefixes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,9 @@
-import { Express, Request, Response } from "express";
+import { Express, Request, Response, Router } from "express";
 import { CreateUserHandler } from "./controllers/user.controller.ts";
 
-export const Routes = (app: Express) => {
-    app.get("/healthcheck", (req: Request, res: Response) => {
-        res.sendStatus(200);
-    });
-    app.post("/signin", CreateUserHandler);
-};
+const router = Router();
+router.get("/healthcheck", (req: Request, res: Response) =>
+    res.sendStatus(200),
+);
+router.post("/user/signup", CreateUserHandler);
+export const Routes = router;
