ba40945a93af3b20c1f208c9418fadd93492b69f
Babar <babarsmoke24@gmail.com>
2025-07-20 18:57:42 +0530

    feat : Implemented Cors to allow all requests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,17 +2,19 @@ import express from "express";
 import config from "config";
 import { ConnectionDB } from "./utils/Connection.ts";
 import { Routes } from "./routes.ts";
+import cors from "cors";
 const PORT = config.get<number>("PORT");
+
 const app = express();
+//Allow all requests
+app.use(cors());
 app.use(express.json());
+app.use("/api/v1/", Routes);
 (async () => {
     console.log("Establishing connection..");
     await ConnectionDB();
-    Routes(app);
     app.listen(PORT, () =>
         console.log(`Server started Successfully at ${PORT}`),
     );
 })();
-//TODO : Might migrate to SQL
-//TODO : User can track theri finances and can have todo pending purchases with nice charts and expected time
-//TODO : Use Router ? but how ?
+//TODO : Implement a Logger
