825fc851224a7f0b2513bf13541f32dc246bf3e4
Babar <babarsmoke24@gmail.com>
2025-07-20 11:27:50 +0530

    feat : Defined a IIFE for quick startup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,18 @@
-//TODO : User can track theri finances and can have todo pending purchases with nice charts and expected time
-//TODO : Might migrate to SQL
 import express from "express";
-const PORT = 9000;
+import config from "config";
+import { ConnectionDB } from "./utils/Connection.ts";
+import { Routes } from "./routes.ts";
+const PORT = config.get<number>("PORT");
 const app = express();
-app.listen(PORT, () => console.log(`Server started Successfully at ${PORT}`));
+app.use(express.json());
+(async () => {
+    console.log("Establishing connection..");
+    await ConnectionDB();
+    Routes(app);
+    app.listen(PORT, () =>
+        console.log(`Server started Successfully at ${PORT}`),
+    );
+})();
+//TODO : Might migrate to SQL
+//TODO : User can track theri finances and can have todo pending purchases with nice charts and expected time
+//TODO : Use Router ? but how ?
