6d4bbc64e4bda47b742fa569797213f3f93fe35c
Babar <babarsmoke24@gmail.com>
2025-07-22 14:49:08 +0530

    Refactor : Routes Updated

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,10 @@
-import { Express, Request, Response, Router } from "express";
+import { Request, Response, Router } from "express";
 import { CreateUserHandler } from "./controllers/user.controller.ts";
-
+import { CreateSessionHandler } from "./controllers/session.controller.ts";
 const router = Router();
 router.get("/healthcheck", (req: Request, res: Response) =>
     res.sendStatus(200),
 );
 router.post("/user/signup", CreateUserHandler);
-router.post("/user/signin");
+router.post("/user/signin", CreateSessionHandler);
 export const Routes = router;
