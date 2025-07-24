07d9ac88dc2709862eeacd0dc417717497903a87
Babar <babarsmoke24@gmail.com>
2025-07-24 08:27:18 +0530

    feat : Account Route Hanlded

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,14 @@ import config from "config";
 import { ConnectionDB } from "./utils/Connection.ts";
 import { Routes } from "./routes.ts";
 import cors from "cors";
+import { DeserializeUser } from "./middlewares/DeserializeUser.ts";
 const PORT = config.get<number>("PORT");
 
 const app = express();
 //Allow all requests
 app.use(cors());
 app.use(express.json());
+app.use(DeserializeUser);
 app.use("/api/v1/", Routes);
 (async () => {
     console.log("Establishing connection..");
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from "express";
 import { CreateUserHandler } from "./controllers/user.controller.ts";
 import { CreateSessionHandler } from "./controllers/session.controller.ts";
+import { CreateAccountHanlder } from "./controllers/Account.controller.ts";
 const router = Router();
 router.get("/healthcheck", (req: Request, res: Response) =>
     res.sendStatus(200),
 );
 router.post("/user/signup", CreateUserHandler);
 router.post("/user/signin", CreateSessionHandler);
+router.get("/user/account", CreateAccountHanlder);
 export const Routes = router;
