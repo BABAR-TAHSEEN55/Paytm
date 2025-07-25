c3978bc83c9474ff397b66a0d1db122ddc285ae2
Babar <babarsmoke24@gmail.com>
2025-07-25 20:39:15 +0530

    feat : Balance Route Handled

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,10 @@
 import { Request, Response, Router } from "express";
 import { CreateUserHandler } from "./controllers/user.controller.ts";
 import { CreateSessionHandler } from "./controllers/session.controller.ts";
-import { CreateAccountHanlder } from "./controllers/Account.controller.ts";
+import {
+    CreateAccountHanlder,
+    GetBalance,
+} from "./controllers/Account.controller.ts";
 const router = Router();
 router.get("/healthcheck", (req: Request, res: Response) =>
     res.sendStatus(200),
@@ -9,4 +12,5 @@ router.get("/healthcheck", (req: Request, res: Response) =>
 router.post("/user/signup", CreateUserHandler);
 router.post("/user/signin", CreateSessionHandler);
 router.get("/user/account", CreateAccountHanlder);
+router.get("/user/balance", GetBalance);
 export const Routes = router;
