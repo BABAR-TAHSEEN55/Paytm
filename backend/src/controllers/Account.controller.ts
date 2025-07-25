debbba21b3f73450c884b4d69106ad97446afad9
Babar <babarsmoke24@gmail.com>
2025-07-25 20:39:24 +0530

    feat : Balance controller implemented

--- backend/src/controllers/Account.controller.ts
+++ backend/src/controllers/Account.controller.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
 import { AuthenticatedRequest } from "../middlewares/DeserializeUser.ts";
 import { CreateAccount } from "../services/Account.Service.ts";
+import AccountModel from "../models/Account.model.ts";
 
 export const CreateAccountHanlder = async (
     req: AuthenticatedRequest,
@@ -16,4 +17,21 @@ export const CreateAccountHanlder = async (
         Balance: 1 + Math.random() + 10000,
     });
     res.send(Account);
+};
+export const GetBalance = async (req: AuthenticatedRequest, res: Response) => {
+    //Get the UserId
+    const UserId = req.user?._id;
+    console.log("User ID :", UserId);
+    if (!UserId) {
+        res.status(400).send("Unauthorized");
+        return;
+    }
+    //Findin the User
+    const Account = await AccountModel.findOne({ UserId });
+    console.log("Account : ", Account);
+    if (!Account) {
+        res.status(404).send("Account doesn't exists");
+        return;
+    }
+    res.send({ Balance: Account.Balance });
 };
