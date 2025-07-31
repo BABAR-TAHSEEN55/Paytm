4ffb312e0c67130c800242b217d2c25d36df88de
Babar <babarsmoke24@gmail.com>
2025-07-31 11:22:33 +0530

    feat : Transaction Route Added

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import { CreateSessionHandler } from "./controllers/session.controller.ts";
 import {
     CreateAccountHanlder,
     GetBalance,
+    Transaction,
 } from "./controllers/Account.controller.ts";
 const router = Router();
 router.get("/healthcheck", (req: Request, res: Response) =>
@@ -13,4 +14,5 @@ router.post("/user/signup", CreateUserHandler);
 router.post("/user/signin", CreateSessionHandler);
 router.get("/user/account", CreateAccountHanlder);
 router.get("/user/balance", GetBalance);
+router.post("/user/transactions", Transaction);
 export const Routes = router;
