29e1a84735120a774d1ccfb98d4964d6d9c3bad4
Babar <babarsmoke24@gmail.com>
2025-07-31 16:06:27 +0530

    feat  : Bulk User Route Implemented

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { CreateUserHandler } from "./controllers/user.controller.ts";
+import { BulkUser, CreateUserHandler } from "./controllers/user.controller.ts";
 import { CreateSessionHandler } from "./controllers/session.controller.ts";
 import {
     CreateAccountHanlder,
@@ -13,6 +13,7 @@ router.get("/healthcheck", (req: Request, res: Response) =>
 router.post("/user/signup", CreateUserHandler);
 router.post("/user/signin", CreateSessionHandler);
 router.get("/user/account", CreateAccountHanlder);
+router.get("/user/bulk", BulkUser);
 router.get("/user/balance", GetBalance);
 router.post("/user/transactions", Transaction);
 export const Routes = router;
