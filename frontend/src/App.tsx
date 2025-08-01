01e23f42c5c3eeb9e94729ddee8a86f81c841e05
Babar <babarsmoke24@gmail.com>
2025-08-01 15:15:27 +0530

    Refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
+import DashBoard from "./Pages/DashBoard";
+import Send from "./Pages/Send";
+import { ToastContainer } from "react-toastify";
+
 const App = () => {
-  return <div className="bg-blue-200 text-2xl">Is this Working?</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/send" element={<Send />} />
+      </Routes>
+      <ToastContainer position="top-right" />
+    </BrowserRouter>
+  );
 };
 export default App;
