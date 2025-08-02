a212bb3a4d4114f79cf721ccc012e6182bbe2361
Babar <babarsmoke24@gmail.com>
2025-08-02 16:17:05 +0530

    feat : Transaction Model Added

---
+++ backend/src/models/Transaction.model.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const TransactionSchema = new mongoose.Schema(
+    {
+        from: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+
+        to: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        amount: {
+            type: Number,
+        },
+        status: {
+            type: String,
+            enum: ["SUCCESS", "FAILURE"],
+            default: "SUCCESS",
+        },
+    },
+    { timestamps: true },
+);
+const TransactionModel = mongoose.model("Transaction", TransactionSchema);
+export default TransactionModel;
