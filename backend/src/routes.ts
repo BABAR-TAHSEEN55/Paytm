72e067f21eb50cfd2ed4aa720510679e146edc31
Babar <babarsmoke24@gmail.com>
2025-08-12 22:59:43 +0530

    feat : New Route added

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,10 @@ import {
     GetTransactionHistory,
     Transaction,
 } from "./controllers/Account.controller.ts";
+import {
+    GetAllRequests,
+    ReceiveHandler,
+} from "./controllers/Receive.controller.ts";
 const router = Router();
 router.get("/healthcheck", (_req: Request, res: Response) =>
     res.sendStatus(200),
@@ -18,4 +22,6 @@ router.get("/user/bulk", BulkUser);
 router.get("/user/balance", GetBalance);
 router.get("/user/history", GetTransactionHistory);
 router.post("/user/transactions", Transaction);
+router.post("/user/request", ReceiveHandler);
+router.get("/user/request", GetAllRequests);
 export const Routes = router;
