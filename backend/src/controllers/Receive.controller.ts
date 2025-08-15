d9e05f8d0000d239630ae7f323ba3850806e828f
Babar <babarsmoke24@gmail.com>
2025-08-15 16:50:57 +0530

    Refactor : Changes etc

--- backend/src/controllers/Receive.controller.ts
+++ backend/src/controllers/Receive.controller.ts
@@ -1,7 +1,10 @@
-import { Request, RequestParamHandler, Response } from "express";
+import { Response } from "express";
 import { CreateRecieve } from "../services/Receive.Service.ts";
 import { AuthenticatedRequest } from "../middlewares/DeserializeUser.ts";
 import ReceiveModel from "../models/Receive.model.ts";
+import mongoose from "mongoose";
+import AccountModel from "../models/Account.model.ts";
+import TransactionModel from "../models/Transaction.model.ts";
 
 export const ReceiveHandler = async (
     req: AuthenticatedRequest,
@@ -21,10 +24,100 @@ export const GetAllRequests = async (
     req: AuthenticatedRequest,
     res: Response,
 ) => {
-    const to = req.user?._id;
+    const from = req.user?._id;
+
+    if (!from) {
+        res.status(400).send("Authorization is Required!");
+        return;
+    }
+
     const IncomingRequest = await ReceiveModel.find({
-        to,
+        to: from,
         status: "pending",
     }).populate("from", "username");
-    res.status(200).send(IncomingRequest);
+
+    const OutGoingRequest = await ReceiveModel.find({
+        from,
+        status: "pending",
+    }).populate("to", "username");
+
+    res.status(200).send({ IncomingRequest, OutGoingRequest });
 };
+export const ResponseToRequest = async (
+    req: AuthenticatedRequest,
+    res: Response,
+) => {
+    //
+    // Starting a Session
+    const to = req.user?._id;
+    const Session = await mongoose.startSession();
+    Session.startTransaction();
+    const User = await ReceiveModel.findOne({
+        to,
+        status: "pending",
+    }).session(Session);
+    if (!User || User.amount == null) {
+        await Session.abortTransaction();
+        res.status(404).send("Request not found");
+        return;
+    }
+    const amount = User.amount;
+    const from = User?.from?._id;
+
+    //This is inside The Mongoose Session
+    const Account = await AccountModel.findOne({ UserId: to }).session(Session);
+
+    if (!Account || Account.Balance < amount) {
+        await Session.abortTransaction();
+        return res
+            .status(400)
+            .json({ message: "Insufficient Balance | Account doesn't exist" });
+    }
+    const toAccount = await AccountModel.findOne({ UserId: from }).session(
+        Session,
+    );
+    if (!toAccount) {
+        await Session.abortTransaction();
+        return res
+            .status(400)
+            .json({ message: "Invalid Account or Account doesn't exist" });
+    }
+    await AccountModel.updateOne(
+        { UserId: to },
+        {
+            $inc: {
+                Balance: -amount,
+            },
+        },
+    ).session(Session);
+    await AccountModel.updateOne(
+        { UserId: from },
+        {
+            $inc: {
+                Balance: amount,
+            },
+        },
+    ).session(Session);
+
+    await TransactionModel.create(
+        {
+            // from: UserId,
+            from: from,
+            to,
+            amount,
+            status: "SUCCESS",
+        },
+        { session: Session },
+    );
+    await ReceiveModel.updateOne(
+        {
+            _id: User._id,
+        },
+        { $set: { status: "accepted" } },
+        { session: Session },
+    );
+
+    await Session.commitTransaction();
+    res.status(200).send("Transaction successfull!!!");
+};
+//TODO: :  1. User Glitching , 2. Avatar Random Color with Random char 3. Zod Check
