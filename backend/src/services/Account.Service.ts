ec1bb6cfc21656f6d8d781d28e7b12d5634e7b72
Babar <babarsmoke24@gmail.com>
2025-08-15 16:51:29 +0530

    Refactor : Services refactored

--- backend/src/services/Account.Service.ts
+++ backend/src/services/Account.Service.ts
@@ -3,7 +3,6 @@ import AccountModel, { AccountDocument } from "../models/Account.model.ts";
 type AccountParams = Pick<AccountDocument, "Balance" | "UserId">;
 export const CreateAccount = async (input: AccountParams) => {
     try {
-        // const User = req.user;
         const Account = await AccountModel.create(input);
         console.log("Account created Successfully");
         return Account?.toJSON();
